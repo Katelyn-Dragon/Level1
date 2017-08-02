4c860ad2eddbd5faa07f7779ae1b6d6c1a216d46
RhymeLyet <rhymeinparentheses@gmail.com>
2017-08-02 11:55:46 -0700

    d

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -20,7 +20,7 @@ public class PhotoQuiz {
 		// image, and select “Copy Image URL”)
 		String Forest = "https://i.ytimg.com/vi/pPrO2jlay40/maxresdefault.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-		String Component = Forest;
+		
 		// 3. use the "createImage()" method below to initialize your Component
 		createImage(Forest);
 		// 4. add the image to the quiz window
@@ -28,8 +28,9 @@ public class PhotoQuiz {
 		// 5. call the pack() method on the quiz window
 		
 		// 6. ask a question that relates to the image
-		
-		String answer = JOptionPane.showInputDialog("What type of habitat is this? " +Forest ); 
+		JOptionPane.showMessageDialog(null, "This Question Will Be Easy");
+		String answer = JOptionPane.showInputDialog("What Type Of Habitat Is This? " + Forest ); 
+		pack(answer);
 		// 7. print "CORRECT" if the user gave the right answer
 		if (answer.equals("rainforest")) {
 			JOptionPane.showMessageDialog(null, "CORRECT");
@@ -41,25 +42,24 @@ public class PhotoQuiz {
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
 
 		// 10. find another image and create it (might take more than one line of code)
-		String Wolf = "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjsucf0yrbVAhUEWCYKHfrgDowQjRwIBw&url=http%3A%2F%2Fwallpapercave.com%2Fwolfpack-wallpaper&psig=AFQjCNF4pGn298JNBFEwoMWnel0OV8MsJA&ust=1501695792866177";
-		createImage(Wolf);
+		String second = "https://qph.ec.quoracdn.net/main-qimg-8911285b5a56a936b8ca047c3dc7da62-c";
+		createImage(second);
 		// 11. add the second image to the quiz window
-
+		
 		// 12. pack the quiz window
 
 		// 13. ask another question
-		pack(Wolf);
-		String animal = JOptionPane.showInputDialog("What animal is this?");
+		JOptionPane.showMessageDialog(null, "This Question Will Be Extremely Easy");
+		String strand = JOptionPane.showInputDialog("What Is The Scientific Name Of This Molecule?" + second);
+		pack(strand);
 		// 14+ check answer, say if correct or incorrect, etc.
-		if (animal.equals("wolf")) {
-			JOptionPane.showMessageDialog(null, "CORRECT");
-		}
-		else if (animal.equals("wolves")) {
+		if (strand.equals("deoxyribonucleic acid")) {
 			JOptionPane.showMessageDialog(null, "CORRECT");
 		}
 		else {
-			JOptionPane.showMessageDialog(null, "INCORRECT");
+			JOptionPane.showMessageDialog(null, "INCORRECT, loser");
 		}
+		
 	}
 
 	private static void pack(String answer) {
