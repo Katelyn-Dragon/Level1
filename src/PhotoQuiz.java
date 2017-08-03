6b73fc46a6a29e5cc153e29d30adb9394c6ce311
RhymeLyet <rhymeinparentheses@gmail.com>
2017-08-03 11:56:32 -0700

    p

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -20,16 +20,16 @@ public class PhotoQuiz {
 		// image, and select “Copy Image URL”)
 		String Forest = "https://i.ytimg.com/vi/pPrO2jlay40/maxresdefault.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-		
+		String Component = Forest;
 		// 3. use the "createImage()" method below to initialize your Component
-		createImage(Forest);
+		createImage(Component);
 		// 4. add the image to the quiz window
 		
 		// 5. call the pack() method on the quiz window
 		
 		// 6. ask a question that relates to the image
 		JOptionPane.showMessageDialog(null, "This Question Will Be Easy");
-		String answer = JOptionPane.showInputDialog("What Type Of Habitat Is This? " + Forest ); 
+		String answer = JOptionPane.showInputDialog("What Type Of Habitat Is This? " + Component ); 
 		pack(answer);
 		// 7. print "CORRECT" if the user gave the right answer
 		if (answer.equals("rainforest")) {
@@ -42,7 +42,7 @@ public class PhotoQuiz {
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
 
 		// 10. find another image and create it (might take more than one line of code)
-		String second = "https://qph.ec.quoracdn.net/main-qimg-8911285b5a56a936b8ca047c3dc7da62-c";
+		String  second= "https://qph.ec.quoracdn.net/main-qimg-8911285b5a56a936b8ca047c3dc7da62-c";
 		createImage(second);
 		// 11. add the second image to the quiz window
 		
@@ -50,16 +50,15 @@ public class PhotoQuiz {
 
 		// 13. ask another question
 		JOptionPane.showMessageDialog(null, "This Question Will Be Extremely Easy");
-		String strand = JOptionPane.showInputDialog("What Is The Scientific Name Of This Molecule?" + second);
+		String strand = JOptionPane.showInputDialog("What Is The Scientific Name For This Molecule?");
 		pack(strand);
 		// 14+ check answer, say if correct or incorrect, etc.
 		if (strand.equals("deoxyribonucleic acid")) {
 			JOptionPane.showMessageDialog(null, "CORRECT");
 		}
 		else {
-			JOptionPane.showMessageDialog(null, "INCORRECT, loser");
+			JOptionPane.showMessageDialog(null, "INCORRECT");
 		}
-		
 	}
 
 	private static void pack(String answer) {
