fb84f4f574b77f2b1b66677ef5475b4498c272af
League <league@WTS8.attlocal.net>
2018-01-29 18:27:48 -0800

    Scoreboard(IP)

--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -3,54 +3,81 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
-import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Scoreboard implements ActionListener {
-public static void main(String[] args) {
-	Scoreboard myScoreboard = new Scoreboard();
-	myScoreboard.run();
-}
-JPanel panel = new JPanel();
-JPanel panel2 = new JPanel();
-JLabel label = new JLabel();
-JButton button = new JButton();
-JButton button2 = new JButton();
-JButton button3 = new JButton();
-int homeScore = 0;
-int awayScore = 0;
+	public static void main(String[] args) {
+		
+		Scoreboard myScoreboard = new Scoreboard();
+		myScoreboard.run();
+		
+	}
 
+	JPanel panel = new JPanel();
+	JPanel panel2 = new JPanel();
+	JPanel mainPanel = new JPanel();
 
-public void run() {
-	JFrame frame = new JFrame();
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setSize(300,300);
-	frame.setVisible(true);
-	button.setText("Home");
-	button2.setText("Away");
-	button.addActionListener(this);
-	button2.addActionListener(this);
-	button.setText(Integer.toString(homeScore));
-	frame.setTitle("Scoreboard");
-	frame.setLayout(new BorderLayout());
-	frame.add(panel, BorderLayout.NORTH);
-	frame.add(panel2, BorderLayout.CENTER);
-	panel.setLayout(new BorderLayout());
-	panel.add(label, BorderLayout.NORTH);
-	panel.add(button, BorderLayout.CENTER);
-	panel.add(button3, BorderLayout.SOUTH);
-	panel2.setLayout(new BorderLayout());
-	panel2.add(button2, BorderLayout.NORTH);
-	frame.pack();
-}
+	JLabel homeLabel = new JLabel();
+	JLabel awayLabel = new JLabel();
+	JLabel hScoreLabel = new JLabel();
+	JLabel aScoreLabel = new JLabel();
+	JLabel hFoulsLabel = new JLabel();
+	JLabel aFoulsLabel = new JLabel();
+	JLabel hTOLabel = new JLabel();
+	JLabel aTOLabel = new JLabel();
 
+	int homeScore = 0;
+	int awayScore = 0;
+	int homeFouls = 0;
+	int awayFouls = 0;
+	int homeTO = 5;
+	int awayTO = 0;
 
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	
-}
+	public void run() {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+		
+		homeLabel.setText("Home");
+		awayLabel.setText("Away");
+
+		hScoreLabel.setText(Integer.toString(homeScore));
+		aScoreLabel.setText(Integer.toString(awayScore));
+		hFoulsLabel.setText(Integer.toString(homeFouls));
+		aFoulsLabel.setText(Integer.toString(awayFouls));
+		hTOLabel.setText(Integer.toString(homeTO));
+		aTOLabel.setText(Integer.toString(awayTO));
+		
+		frame.setTitle("Scoreboard");
+
+		mainPanel.add(panel);
+		mainPanel.add(panel2);
+		
+		frame.setLayout(new BorderLayout());
+		frame.add(mainPanel);
+
+		panel.setLayout(new BorderLayout());
+		panel.add(homeLabel, BorderLayout.NORTH);
+		panel.add(hScoreLabel, BorderLayout.SOUTH);
+		panel.add(hFoulsLabel, BorderLayout.CENTER);
+		panel.add(hTOLabel, BorderLayout.SOUTH);
+		
+		panel2.setLayout(new BorderLayout());
+		panel2.add(awayLabel, BorderLayout.NORTH);
+		panel2.add(aScoreLabel, BorderLayout.SOUTH);
+		panel2.add(aFoulsLabel, BorderLayout.CENTER);
+		
+		frame.pack();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 }
