0674157395ae509706900414b281f3355a15c0d1
League Student <league@iMac-4.attlocal.net>
2018-04-23 18:29:40 -0700

    GamePanel(IP), LeagueInvaders(IP)

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,49 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GamePanel {
+import javax.swing.Timer;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener, KeyListener{
+Timer timer;
+
+public GamePanel() {
+	timer = new Timer(1000/6, this);
+}
+
+public void startGame() {
+	timer.start();
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	repaint();
+}
+@Override
+public void paintComponent(Graphics g) {
+	g.fillRect(10, 10, 100, 100);
+}
+
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	System.out.println("typed");
+}
+
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	System.out.println("pressed");
+}
+
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	System.out.println("released");
+}
 
 }
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,3 +1,5 @@
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class LeagueInvaders {
@@ -9,13 +11,19 @@ public static void main(String[] args) {
 JFrame gameFrame;
 final int width = 500;
 final int height = 800;
+GamePanel gamePanel;
 
 public LeagueInvaders() {
 	gameFrame = new JFrame();
+	gamePanel = new GamePanel();
 }
 
 public void setup() {
 	gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	gameFrame.add(gamePanel);
+	gameFrame.setVisible(true);
+	gameFrame.getContentPane().setPreferredSize(new Dimension(width, height));
+	gameFrame.pack();
+	gamePanel.startGame();
 }
-
 }
